7fd064dc9ba91e663e3e868a1c365f5c993bac91
Kavi-Jain-2003 <kavijain220803@gmail.com>
2026-02-20 11:20:31 +0530

    [KaviJain] adding uc2 feet and inches measurement equality

--- FeatEquality/quantitymeasurement/src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ FeatEquality/quantitymeasurement/src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -1,58 +1,96 @@
 package com.apps.quantitymeasurement;
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class QuantityMeasurementAppTest {
 
+    //uc1
+
     @Test
-    void testEquality_SameValue() {
-        QuantityMeasurementApp.Feet f1 =
-                new QuantityMeasurementApp.Feet(1.0);
-
-        QuantityMeasurementApp.Feet f2 =
-                new QuantityMeasurementApp.Feet(1.0);
-
-        assertTrue(f1.equals(f2),
-                "1.0 ft should equal 1.0 ft");
+    void testFeetEquality_SameValue() {
+        assertTrue(
+            QuantityMeasurementApp.checkFeetEquality(1.0, 1.0),
+            "1.0 ft should equal 1.0 ft"
+        );
     }
 
     @Test
-    void testEquality_DifferentValue() {
-        QuantityMeasurementApp.Feet f1 =
-                new QuantityMeasurementApp.Feet(1.0);
-
-        QuantityMeasurementApp.Feet f2 =
-                new QuantityMeasurementApp.Feet(2.0);
-
-        assertFalse(f1.equals(f2),
-                "1.0 ft should not equal 2.0 ft");
+    void testFeetEquality_DifferentValue() {
+        assertFalse(
+            QuantityMeasurementApp.checkFeetEquality(1.0, 2.0),
+            "1.0 ft should not equal 2.0 ft"
+        );
     }
 
     @Test
-    void testEquality_NullComparison() {
-        QuantityMeasurementApp.Feet f1 =
+    void testFeetEquality_SameReference() {
+        QuantityMeasurementApp.Feet feet =
                 new QuantityMeasurementApp.Feet(1.0);
 
-        assertFalse(f1.equals(null),
-                "Feet should not equal null");
-    }
-
-    @Test
-    void testEquality_SameReference() {
-        QuantityMeasurementApp.Feet f1 =
-                new QuantityMeasurementApp.Feet(1.0);
-
-        assertTrue(f1.equals(f1),
+        assertTrue(feet.equals(feet),
                 "Object should equal itself (Reflexive)");
     }
 
     @Test
-    void testEquality_NonNumericInput() {
-        QuantityMeasurementApp.Feet f1 =
+    void testFeetEquality_NullComparison() {
+        QuantityMeasurementApp.Feet feet =
                 new QuantityMeasurementApp.Feet(1.0);
 
-        assertFalse(f1.equals("abc"),
+        assertFalse(feet.equals(null),
+                "Feet should not equal null");
+    }
+
+    @Test
+    void testFeetEquality_NonNumericInput() {
+        QuantityMeasurementApp.Feet feet =
+                new QuantityMeasurementApp.Feet(1.0);
+
+        assertFalse(feet.equals("abc"),
                 "Feet should not equal non-numeric input");
+    }
+
+  //uc2
+
+    @Test
+    void testInchEquality_SameValue() {
+        assertTrue(
+            QuantityMeasurementApp.checkInchesEquality(1.0, 1.0),
+            "1.0 inch should equal 1.0 inch"
+        );
+    }
+
+    @Test
+    void testInchEquality_DifferentValue() {
+        assertFalse(
+            QuantityMeasurementApp.checkInchesEquality(1.0, 2.0),
+            "1.0 inch should not equal 2.0 inch"
+        );
+    }
+
+    @Test
+    void testInchEquality_SameReference() {
+        QuantityMeasurementApp.Inches inch =
+                new QuantityMeasurementApp.Inches(1.0);
+
+        assertTrue(inch.equals(inch),
+                "Object should equal itself (Reflexive)");
+    }
+
+    @Test
+    void testInchEquality_NullComparison() {
+        QuantityMeasurementApp.Inches inch =
+                new QuantityMeasurementApp.Inches(1.0);
+
+        assertFalse(inch.equals(null),
+                "Inch should not equal null");
+    }
+
+    @Test
+    void testInchEquality_NonNumericInput() {
+        QuantityMeasurementApp.Inches inch =
+                new QuantityMeasurementApp.Inches(1.0);
+
+        assertFalse(inch.equals("xyz"),
+                "Inch should not equal non-numeric input");
     }
 }

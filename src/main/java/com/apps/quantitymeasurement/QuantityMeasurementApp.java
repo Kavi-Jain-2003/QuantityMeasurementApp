c5e778b5a4df86becc05bdc9e18db60ce7fdfb19
Kavi-Jain-2003 <kavijain220803@gmail.com>
2026-03-10 14:35:33 +0530

    [KaviJain] adding uc11

---
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -0,0 +1,37 @@
+package com.apps.quantitymeasurement;
+
+public class QuantityMeasurementApp {
+
+    public static void main(String[] args) {
+
+//        Quantity<LengthUnit> feet = new Quantity<>(1.0, LengthUnit.FEET);
+//        Quantity<LengthUnit> inches = new Quantity<>(12.0, LengthUnit.INCH);
+//
+//        System.out.println("1 Foot equals 12 Inches: " + feet.equals(inches));
+//
+//        Quantity<WeightUnit> kg = new Quantity<>(1.0, WeightUnit.KILOGRAM);
+//        Quantity<WeightUnit> g = new Quantity<>(1000.0, WeightUnit.GRAM);
+//
+//        System.out.println("1 kg equals 1000 g: " + kg.equals(g));
+//
+//        Quantity<WeightUnit> sum = kg.add(new Quantity<>(500.0, WeightUnit.GRAM));
+//        System.out.println("Sum in default unit: " + sum);
+//
+//        Quantity<WeightUnit> sumInLb = kg.add(new Quantity<>(500.0, WeightUnit.GRAM), WeightUnit.POUND);
+//        System.out.println("Sum in Pounds: " + sumInLb);
+//        
+        Quantity<VolumeUnit> v1 = new Quantity<>(1.0, VolumeUnit.LITRE);
+        Quantity<VolumeUnit> v2 = new Quantity<>(1000.0, VolumeUnit.MILLILITRE);
+        Quantity<VolumeUnit> v3 = new Quantity<>(1.0, VolumeUnit.GALLON);
+
+        System.out.println("1 L = 1000 mL: " + v1.equals(v2));
+        System.out.println("1 Gallon in Litres: " + v3.convertTo(VolumeUnit.LITRE));
+
+        Quantity<VolumeUnit> sum = v1.add(v2);
+        System.out.println("Sum: " + sum);
+
+        Quantity<VolumeUnit> sumMl = v1.add(v3, VolumeUnit.MILLILITRE);
+        System.out.println("Sum in mL: " + sumMl);
+
+    }
+}

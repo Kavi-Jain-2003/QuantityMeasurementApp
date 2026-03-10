114f6720083c35466d671120643260d168dca6ce
Kavi-Jain-2003 <kavijain220803@gmail.com>
2026-03-10 23:47:58 +0530

    [KaviJain] refactor uc4

--- FeatEquality/quantitymeasurement/src/main/java/com/apps/quantitymeasurement/LengthUnit.java
+++ FeatEquality/quantitymeasurement/src/main/java/com/apps/quantitymeasurement/LengthUnit.java
@@ -3,9 +3,8 @@ package com.apps.quantitymeasurement;
 public enum LengthUnit {
 
     FEET(1.0),           // Base unit
-    INCH(1.0 / 12.0),  // 1 inch = 1/12 feet
-    YARD(3.0),                         // 1 yard = 3 feet
-    CENTIMETER(0.0328084167);          // 1 cm = 0.0328084167 feet
+    INCH(1.0 / 12.0);  // 1 inch = 1/12 feet
+    
 
     private final double conversionFactorToFeet;
 

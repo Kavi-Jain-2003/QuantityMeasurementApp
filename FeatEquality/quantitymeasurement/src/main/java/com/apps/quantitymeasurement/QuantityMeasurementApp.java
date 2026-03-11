4c160dd05f4ef1b664a670f19c48137453867188
Kavi-Jain-2003 <kavijain220803@gmail.com>
2026-03-11 10:58:00 +0530

    [KaviJain] refactoring uc8

--- FeatEquality/quantitymeasurement/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ FeatEquality/quantitymeasurement/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -4,15 +4,71 @@ public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
+        demonstrateEquality();
+        demonstrateConversion();
+        demonstrateAddition();
+        demonstrateAdditionWithTargetUnit();
+        demonstrateUnitConversionMethods();
+        demonstrateCrossUnitEquality();
+    }
+
+    // 1
+    static void demonstrateEquality() {
+
         Length l1 = new Length(1, LengthUnit.FEET);
         Length l2 = new Length(12, LengthUnit.INCHES);
 
         System.out.println("Equality: " + l1.equals(l2));
+    }
 
-        System.out.println("Conversion: " + l1.convertTo(LengthUnit.INCHES));
+    // 2
+    static void demonstrateConversion() {
 
-        System.out.println("Addition: " + l1.add(l2));
+        Length l = new Length(1, LengthUnit.FEET);
 
-        System.out.println("Addition in yards: " + l1.add(l2, LengthUnit.YARDS));
+        Length result = l.convertTo(LengthUnit.INCHES);
+
+        System.out.println("Conversion: " + result);
+    }
+
+    // 3
+    static void demonstrateAddition() {
+
+        Length l1 = new Length(1, LengthUnit.FEET);
+        Length l2 = new Length(12, LengthUnit.INCHES);
+
+        Length result = l1.add(l2);
+
+        System.out.println("Addition: " + result);
+    }
+
+    // 4
+    static void demonstrateAdditionWithTargetUnit() {
+
+        Length l1 = new Length(1, LengthUnit.FEET);
+        Length l2 = new Length(12, LengthUnit.INCHES);
+
+        Length result = l1.add(l2, LengthUnit.YARDS);
+
+        System.out.println("Addition with target unit: " + result);
+    }
+
+    // 5
+    static void demonstrateUnitConversionMethods() {
+
+        double base = LengthUnit.INCHES.convertToBaseUnit(12);
+        double inches = LengthUnit.INCHES.convertFromBaseUnit(1);
+
+        System.out.println("12 inches to feet: " + base);
+        System.out.println("1 foot to inches: " + inches);
+    }
+
+    // 6
+    static void demonstrateCrossUnitEquality() {
+
+        Length l1 = new Length(36, LengthUnit.INCHES);
+        Length l2 = new Length(1, LengthUnit.YARDS);
+
+        System.out.println("36 inches equals 1 yard: " + l1.equals(l2));
     }
 }
--- FeatEquality/quantitymeasurement/src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ FeatEquality/quantitymeasurement/src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -5,40 +5,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class QuantityMeasurementAppTest {
 
-    // UC1
     @Test
     void testFeetEquality() {
-
-        Length l1 = new Length(1, LengthUnit.FEET);
-        Length l2 = new Length(1, LengthUnit.FEET);
-
-        assertTrue(l1.equals(l2));
+        assertTrue(new Length(1, LengthUnit.FEET)
+                .equals(new Length(1, LengthUnit.FEET)));
     }
 
-    // UC2
     @Test
-    void testFeetInchEquality() {
-
-        Length l1 = new Length(1, LengthUnit.FEET);
-        Length l2 = new Length(12, LengthUnit.INCHES);
-
-        assertTrue(l1.equals(l2));
+    void testFeetInchesEquality() {
+        assertTrue(new Length(1, LengthUnit.FEET)
+                .equals(new Length(12, LengthUnit.INCHES)));
     }
 
-    // UC3
     @Test
     void testDifferentLength() {
-
-        Length l1 = new Length(1, LengthUnit.FEET);
-        Length l2 = new Length(2, LengthUnit.FEET);
-
-        assertFalse(l1.equals(l2));
+        assertFalse(new Length(1, LengthUnit.FEET)
+                .equals(new Length(2, LengthUnit.FEET)));
     }
 
-    // UC4
     @Test
     void testFeetToInchesConversion() {
-
         assertEquals(12,
                 Length.convert(1, LengthUnit.FEET, LengthUnit.INCHES),
                 0.0001);
@@ -46,41 +32,13 @@ class QuantityMeasurementAppTest {
 
     @Test
     void testInchesToFeetConversion() {
-
         assertEquals(2,
                 Length.convert(24, LengthUnit.INCHES, LengthUnit.FEET),
                 0.0001);
     }
 
     @Test
-    void testCentimeterToInchConversion() {
-
-        assertEquals(1,
-                Length.convert(2.54, LengthUnit.CENTIMETERS, LengthUnit.INCHES),
-                0.0001);
-    }
-
-    @Test
-    void testInvalidUnit() {
-
-        assertThrows(IllegalArgumentException.class, () ->
-                Length.convert(1, null, LengthUnit.FEET));
-    }
-
-    // UC6
-    @Test
-    void testAddition_SameUnit_FeetPlusFeet() {
-
-        Length l1 = new Length(1, LengthUnit.FEET);
-        Length l2 = new Length(2, LengthUnit.FEET);
-
-        Length result = l1.add(l2);
-
-        assertEquals(new Length(3, LengthUnit.FEET), result);
-    }
-
-    @Test
-    void testAddition_CrossUnit_FeetPlusInches() {
+    void testAddition() {
 
         Length l1 = new Length(1, LengthUnit.FEET);
         Length l2 = new Length(12, LengthUnit.INCHES);
@@ -91,19 +49,7 @@ class QuantityMeasurementAppTest {
     }
 
     @Test
-    void testAddition_WithZero() {
-
-        Length l1 = new Length(5, LengthUnit.FEET);
-        Length l2 = new Length(0, LengthUnit.INCHES);
-
-        Length result = l1.add(l2);
-
-        assertEquals(new Length(5, LengthUnit.FEET), result);
-    }
-
-    // UC7
-    @Test
-    void testAddition_WithTargetUnit() {
+    void testAdditionWithTargetUnit() {
 
         Length l1 = new Length(1, LengthUnit.FEET);
         Length l2 = new Length(12, LengthUnit.INCHES);
@@ -113,18 +59,15 @@ class QuantityMeasurementAppTest {
         assertEquals(new Length(0.6667, LengthUnit.YARDS), result);
     }
 
-    // UC8
     @Test
-    void testConvertToBaseUnit_InchesToFeet() {
-
+    void testConvertToBaseUnit() {
         assertEquals(1,
                 LengthUnit.INCHES.convertToBaseUnit(12),
                 0.0001);
     }
 
     @Test
-    void testConvertFromBaseUnit_FeetToInches() {
-
+    void testConvertFromBaseUnit() {
         assertEquals(12,
                 LengthUnit.INCHES.convertFromBaseUnit(1),
                 0.0001);
@@ -133,17 +76,10 @@ class QuantityMeasurementAppTest {
     @Test
     void testConvertToMethod() {
 
-        Length length = new Length(1, LengthUnit.FEET);
+        Length l = new Length(1, LengthUnit.FEET);
 
-        Length result = length.convertTo(LengthUnit.INCHES);
+        Length result = l.convertTo(LengthUnit.INCHES);
 
         assertEquals(new Length(12, LengthUnit.INCHES), result);
-    }
-
-    @Test
-    void testInvalidValue() {
-
-        assertThrows(IllegalArgumentException.class, () ->
-                new Length(Double.NaN, LengthUnit.FEET));
     }
 }

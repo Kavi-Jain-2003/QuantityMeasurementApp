013cb63451d8b41174cf40b79a9a6cbe344a85a5
Kavi-Jain-2003 <kavijain220803@gmail.com>
2026-03-23 23:46:38 +0530

    [KaviJain] refactoring uc10

--- FeatEquality/quantitymeasurement/src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ FeatEquality/quantitymeasurement/src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -3,7 +3,7 @@ package com.apps.quantitymeasurement;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class QuantityTest {
+class QuantityMeasurementAppTest {
 
     @Test
     void testLengthEquality() {
